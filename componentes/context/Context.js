432353da0e701e76acdfa047c833d19809fbb53d
EdwinMLara <Valiant.ecatepec1>
2021-07-23 15:24:28 -0500

    agregando componentes de control

--- componentes/context/Context.js
+++ componentes/context/Context.js
@@ -47,7 +47,17 @@ const Request = {
 
 const State = {
     loading:false,
-    logging:false
+    logging:false,
+    seconds2Time : (seconds) => {
+        let hours = Math.floor(seconds/3600);
+        let min = Math.floor((seconds - (hours * 3600)) / 60);
+        let sec = seconds - (hours * 3600) - (min * 60);
+        if (hours   < 10) {hours   = "0"+hours;}
+        if (min < 10) {min = "0" + min;}
+        if (sec < 10) {sec = "0" + sec;}
+        return hours+':'+min+':'+sec;
+    },
+    defase:0
 }
 
 export const appStateContext = {
