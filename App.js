6f1e6b09fb301584a6292600c972befbb5abf0bb
EdwinMLara <Valiant.ecatepec1>
2021-07-26 15:30:36 -0500

    Agregando el navegador

--- App.js
+++ App.js
@@ -1,35 +1,42 @@
-import React,{useState} from 'react';
-import {View} from 'react-native'
+import React, {useState} from 'react';
+import {View} from 'react-native';
 
-import {
-  ActivityIndicator,
-  StatusBar,
-  ImageBackground
-} from 'react-native';
+import {ActivityIndicator, StatusBar, ImageBackground} from 'react-native';
 
-import {Context,appStateContext} from './componentes/context/Context'
+import {Context, appStateContext} from './componentes/context/Context';
 import stylesApp from './stylesApp';
 import plaza from './assest/plaza.jpg';
 import Login from './componentes/LoginScreem/login';
 import Control from './componentes/controlScreen/Control';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-
+const Stack = createStackNavigator();
 
 const App = () => {
   const [context, setContext] = useState(appStateContext);
 
   return (
     <View style={stylesApp.mainContainer}>
-      <StatusBar backgroundColor="#9a0007"/>
-      <ImageBackground 
-        style={stylesApp.imageBackground} 
+      <StatusBar backgroundColor="#9a0007" />
+      <ImageBackground
+        style={stylesApp.imageBackground}
         source={plaza}
         resizeMode="cover">
-      <Context.Provider value={[context,setContext]}>
-          <Control/>
-          {/*<Login/>
-        <ActivityIndicator animating={context.appState.loading} style={stylesApp.spinner}size="large" color="red" />*/}
-      </Context.Provider>
+        <NavigationContainer headerMode="none">
+          <Context.Provider value={[context, setContext]}>
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false,
+              }}
+              initialRouteName="Login">
+              <Stack.Screen name="Login" component={Login} />
+              <Stack.Screen name="Control" component={Control} />
+            </Stack.Navigator>
+
+            {/* <ActivityIndicator animating={context.appState.loading} style={stylesApp.spinner}size="large" color="red" />*/}
+          </Context.Provider>
+        </NavigationContainer>
       </ImageBackground>
     </View>
   );
